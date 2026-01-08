cfb32ad669833a50f8276d100e237df3a51b6a8a
gaucho33 <162388686+gaucho33@users.noreply.github.com>
2026-01-08 14:24:32 -0300

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,17 @@
-
 import React from 'react';
+// Cambiamos la ruta para que coincida exactamente con el importmap
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("No se encontró el elemento raíz 'root'. La interpelación no puede comenzar.");
 }
 
+// Inicializamos el asombro
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
